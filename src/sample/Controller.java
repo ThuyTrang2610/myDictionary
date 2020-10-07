71d8c2261a7fa12d95083355268debfc4e7a54d2
ThuyTrang2610 <tranghoanganhbaby@gmail.com>
2020-10-07 17:20:40 +0700

    update 7/10/2020 (2)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,15 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
-import java.awt.*;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,7 +20,7 @@ public class Controller implements Initializable{
     public Button btSearch;
 
     @FXML
-    public Button btclear;
+    public Button btClear;
     @FXML
     public TextField tfSearch;
 
@@ -27,21 +30,30 @@ public class Controller implements Initializable{
     @FXML
     public TextArea taMean;
 
+    @FXML
+    public ContextMenu cmSearch;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        btclear.setOnMouseClicked(mouseEvent -> {
+        btClear.setOnMouseClicked(event -> {
             tfSearch.clear();
         });
 
-        btSearch.setOnMouseClicked(this::handle);
+       btSearch.setOnMouseClicked(event -> {
+            String tmp = tfSearch.getText();
+            taMean.setText(Main.dic.get(tmp));
+        });
 
+        tfSearch.setContextMenu(cmSearch);
+        tfSearch.setOnKeyTyped(keyEvent -> {
+            String tmp = tfSearch.getText();
+            MenuItem a = new MenuItem(tmp);
+            cmSearch.getItems().add(a);
+            cmSearch.show(tfSearch, Side.BOTTOM, 0, 0);
+        });
     }
 
-    public void handle(MouseEvent mouseEvent) {
-        tfSearch.clear();// xoa van ban
-        String tmp = tfSearch.getText();
 
-    }
 }
 
